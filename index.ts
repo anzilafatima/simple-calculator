2446891122541068883396ffd9154360f93fbb68
anzilafatima <165508713+anzilafatima@users.noreply.github.com>
2024-04-10 02:46:59 +0500

    my first commit

---
+++ index.ts
@@ -0,0 +1,37 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer"; 
+
+const answer = await inquirer.prompt([
+    {
+        message: "enter first number",
+        type:"number",
+        name:"firstnumber",
+    },
+    {
+        message:"enter second number",
+        type:"number",
+        name:"secondnumber"
+
+    }, 
+    {
+        message:"select one of these operators",
+        type:"list",
+        name:"operator",
+        choices:["addition","subtraction","multiplication","division"]
+    },
+]);
+if(answer.operator === "addition"){
+    console.log(answer.firstnumber + answer.secondnumber)
+} 
+else if(answer.operator === "subtraction"){
+    console.log(answer.firstnumber - answer.secondnumber)
+} 
+else if(answer.operator === "multiplication"){
+    console.log(answer.firstnumber * answer.secondnumber)
+} 
+else if(answer.operator === "division"){
+    console.log(answer.firstnumber / answer.secondnumber)
+} 
+else {
+    console.log("invailid operator")
+}
